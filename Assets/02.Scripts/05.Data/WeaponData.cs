ca072c9cdc0e9c89ca7298b770b8ccf80b227490
Cafor <formuloratio@gmail.com>
2025-12-04 12:33:43 +0900

    feat: 장비 아이템 추가

--- Assets/02.Scripts/05.Data/WeaponData.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Weapon", menuName = "Game Data/Weapon")]
-public class WeaponData : ScriptableObject
-{
-    public string weaponName;
-    public Sprite icon;
-    public GameObject WeaponPrefab;
-}
